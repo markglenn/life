1457d3dd91092b91937af18d737f45f2f229ff66
Mark Glenn <markglenn@gmail.com>
2014-09-25 12:36:19 -0500

    Add GLM and googlemock

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "glm/vec3.hpp"
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    glm::vec3 example = glm::vec3(0.0f, 1.0f, 2.0f);
+    std::cout << "Hello World: " << example.y << std::endl;
     return 0;
 }
