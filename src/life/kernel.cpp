51e09fe3c0d11946e22992a7c4aa58db98dcfeaa
Mark Glenn <markglenn@gmail.com>
2014-10-02 22:39:30 -0500

    Start service when adding to kernel

--- src/life/kernel.cpp
+++ src/life/kernel.cpp
@@ -23,15 +23,20 @@ namespace life
     bool kernel::add_service( service* service )
     //////////////////////////////////////////////////////////////////////////
     {
+        if (!service->start( ) )
+            return false;
+
+        int priority = service->priority( );
+
         // Find where to put the service
         auto pos = std::find_if(
             _services.begin(),
             _services.end(),
-            [=]( auto s ) -> bool { return s->priority() > service->priority(); }
+            [priority]( auto s ) -> bool { return s->priority() > priority; }
         );
 
         _services.insert(pos, service);
 
-        return false;
+        return true;
     }
 }
--- test/life/kernel_tests.cpp
+++ test/life/kernel_tests.cpp
@@ -2,12 +2,20 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+using ::testing::Return;
+
 class mock_service : public life::service
 {
 public:
-    mock_service( int priority ) : service( priority ){}
+    mock_service( int priority ) : service( priority )
+    {
+        // Default values for mock
+        ON_CALL(*this, start()).WillByDefault(Return(true));
+    }
+
     MOCK_METHOD1(update, bool (const life::gametime& gametime));
     MOCK_CONST_METHOD0(name, std::string());
+    MOCK_METHOD0(start, bool());
 };
 
 TEST(kernel_constructor, starts_with_no_services) {
@@ -23,7 +31,7 @@ TEST(kernel_add_service, adds_service_to_list) {
     EXPECT_EQ(1, (*kernel.cbegin())->priority());
 }
 
-TEST(kernel_add_service_test, adds_service_to_list_by_priority) {
+TEST(kernel_add_service, adds_service_to_list_by_priority) {
     life::kernel kernel;
 
     kernel.add_service( new mock_service( 2 ) );
@@ -33,4 +41,38 @@ TEST(kernel_add_service_test, adds_service_to_list_by_priority) {
 
     EXPECT_EQ((*result++)->priority(), 1);
     EXPECT_EQ((*result++)->priority(), 2);
+}
+
+TEST(kernel_add_service, runs_start_on_service) {
+    life::kernel kernel;
+    auto service = new mock_service(1);
+
+    EXPECT_CALL( *service, start())
+        .WillOnce(Return(true));
+
+    kernel.add_service( service );
+}
+
+TEST(kernel_add_service, adds_service_if_start_returns_true) {
+    life::kernel kernel;
+    auto service = new mock_service(1);
+
+    EXPECT_CALL( *service, start())
+        .WillOnce(Return(true));
+
+    ASSERT_TRUE( kernel.add_service( service ) );
+    EXPECT_EQ(1, kernel.service_count());
+}
+
+TEST(kernel_add_service, does_not_add_service_if_start_returns_false) {
+    life::kernel kernel;
+    auto service = new mock_service(1);
+
+    EXPECT_CALL( *service, start())
+        .WillOnce(Return(false));
+
+    ASSERT_FALSE( kernel.add_service( service ) );
+    EXPECT_EQ(0, kernel.service_count());
+
+    delete service;
 }
