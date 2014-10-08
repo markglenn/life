c8423e6079818dee54adeb212672b4c8d7c6668f
Mark Glenn <markglenn@gmail.com>
2014-10-07 21:48:38 -0500

    Cleanup

--- src/life/sdl_istream_adapter.cpp
+++ src/life/sdl_istream_adapter.cpp
@@ -2,45 +2,61 @@
 
 namespace life
 {
+    ///////////////////////////////////////////////////////////////////////////
     Sint64 istream_seek( struct SDL_RWops *context, Sint64 offset, int whence)
+    ///////////////////////////////////////////////////////////////////////////
     {
         auto stream = ( std::istream* )context->hidden.unknown.data1;
 
-        if ( whence == SEEK_SET )
-            stream->seekg( offset, std::ios::beg );
-        else if ( whence == SEEK_CUR )
-            stream->seekg( offset, std::ios::cur );
-        else if ( whence == SEEK_END )
-             stream->seekg( offset, std::ios::end );
+        switch( whence )
+        {
+            case SEEK_SET:
+                stream->seekg( offset, std::ios::beg );
+                break;
+
+            case SEEK_CUR:
+                stream->seekg( offset, std::ios::cur );
+                break;
+
+            case SEEK_END:
+                stream->seekg( offset, std::ios::end );
+                break;
+        }
 
         return stream->fail( ) ? -1 : ( int )stream->tellg( );
     }
 
-
-    size_t istream_read(SDL_RWops *context, void *ptr, size_t size, size_t maxnum)
+    ///////////////////////////////////////////////////////////////////////////
+    size_t istream_read( SDL_RWops *context, void *ptr, size_t size, size_t maxnum )
+    ///////////////////////////////////////////////////////////////////////////
     {
-        if ( size == 0 ) return -1;
-        std::istream* stream = (std::istream*) context->hidden.unknown.data1;
-        stream->read( (char*)ptr, size * maxnum );
+        // Exit if we're not reading anything
+        if ( size == 0 )
+            return -1;
 
-        return stream->bad() ? -1 : stream->gcount() / size;
+        auto stream = ( std::istream* )context->hidden.unknown.data1;
+        stream->read( static_cast<char*>( ptr ), size * maxnum );
+
+        return stream->bad( ) ? -1 : stream->gcount( ) / size;
     }
 
+    ///////////////////////////////////////////////////////////////////////////
     int istream_close( SDL_RWops *context )
+    ///////////////////////////////////////////////////////////////////////////
     {
-        if ( context ) {
+        if ( context )
             SDL_FreeRW( context );
-        }
+
         return 0;
     }
 
-
+    ///////////////////////////////////////////////////////////////////////////
     SDL_RWops *SDL_RWFromIStream( std::istream& stream )
+    ///////////////////////////////////////////////////////////////////////////
     {
-        SDL_RWops *rwops;
-        rwops = SDL_AllocRW();
+        SDL_RWops* rwops = SDL_AllocRW( );
 
-        if ( rwops != NULL ) 
+        if( rwops )
         {
             rwops->seek = istream_seek;
             rwops->read = istream_read;
@@ -48,6 +64,7 @@ namespace life
             rwops->close = istream_close;
             rwops->hidden.unknown.data1 = &stream;
         }
+
         return rwops;
     }
 }
